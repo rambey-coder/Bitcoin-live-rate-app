792210e92e4c351823d4b32d908b960bd70256c7
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-04-05 12:06:53 +0100

    props passed and value display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
  import React, { useState, useEffect } from 'react'
  import './App.css';
  import axios from 'axios'
+import Coin from './Coin';
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -13,7 +15,15 @@ function App() {
     })
 
     .catch(err => console.log(err))
-  },[])
+  },[]);
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const filterCoins = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLocaleLowerCase())
+    )
 
   return (
     <div className="coin-app">
@@ -22,11 +32,23 @@ function App() {
         <form>
           <input type="text"
            className="coin-input"
-           placeholder='Search' />
+           placeholder='Search'
+           onChange={handleChange} />
         </form>
       </div>
 
-      
+    {
+      filterCoins.map(coin => {
+        return (
+          <Coin key={coin.id}
+          name={coin.name}
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.market_cap}
+          price={coin.current_price}/>
+        )
+      })
+    }
     </div>
   );
 }
