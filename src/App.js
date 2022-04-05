4eff49ef22712c0e1471d9067f7b1b6c4ed091da
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-04-05 11:52:42 +0100

    values added dynamically using props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+ import React, { useState, useEffect } from 'react'
+ import './App.css';
+ import axios from 'axios'
 
 function App() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+
+    .then (res => {
+      setCoins(res.data);
+    })
+
+    .catch(err => console.log(err))
+  },[])
+
   return (
-    <div className="App">
-      <h1>apii</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text"
+           className="coin-input"
+           placeholder='Search' />
+        </form>
+      </div>
+
+      
     </div>
   );
 }
